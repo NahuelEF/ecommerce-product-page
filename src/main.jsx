1081500c61b9557b885f552e8f1e93d2b3a0c068
NahuelEF <nahuelfedele@gmail.com>
2023-11-01 03:12:36 -0300

    add: custom palette and typography

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,21 @@ import App from "./App.jsx";
 import "./index.css";
 
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ffffff",
+      light: "#f7f8fd",
+    },
+    secondary: {
+      main: "#ff7d1a",
+      light: "#ffede0",
+      contrastText: "#ffffff",
+    },
+    text: {
+      primary: "#1d2025",
+      secondary: "#68707d",
+    },
+  },
   typography: {
     fontFamily: "'Kumbh Sans', sans-serif",
     fontSize: 16,
